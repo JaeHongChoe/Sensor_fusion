8a5d796d22e0a882dbc507b8e6f6a1f4aa30fa69
JaeHong Choe <65914440+JaeHongChoe@users.noreply.github.com>
2024-02-22 13:57:56 +0900

    Add files via upload

---
+++ xycar_sensor_fusion/sensor_fusion/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nvidia/xycar_ws/src/sensor_fusion/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/nvidia/xycar_ws/src/sensor_fusion/build/devel/lib:/home/nvidia/xycar_ws/devel/lib:/opt/ros/melodic/lib:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda-10.2/lib64'
+export PATH='/opt/ros/melodic/bin:/home/nvidia/.vscode-server/bin/8b3775030ed1a69b13e4f4c628c612102e30a681/bin/remote-cli:/home/nvidia/.local/bin:/usr/local/cuda-10.2/bin:/home/nvidia/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/loacl/cuda-10.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/nvidia/xycar_ws/src/sensor_fusion/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/nvidia/xycar_ws/src/sensor_fusion/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nvidia/xycar_ws/src/sensor_fusion/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/nvidia/xycar_ws/src/sensor_fusion:$ROS_PACKAGE_PATH"
